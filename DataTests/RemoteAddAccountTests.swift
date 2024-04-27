3bfc570790061bd9ecbfd2f7dc61cfe5a58dc786
Leticia <leticiaspeda@gmail.com>
2024-04-27 19:30:38 -0300

    test: garantindo que o RemoteAccount esta chamando HttpClient com o data correto

--- DataTests/RemoteAddAccountTests.swift
+++ DataTests/RemoteAddAccountTests.swift
@@ -1,4 +1,5 @@
 import XCTest
+@testable import Domain
 
 
 class RemoteAddAccount {
@@ -10,13 +11,14 @@ class RemoteAddAccount {
         self.httpClient = httpClient
     }
     
-    func add() {
-        httpClient.post(url: url)
+    func add(addAccountModel: AddAccountModel) {
+        let data = try? JSONEncoder().encode(addAccountModel)
+        httpClient.post(to: url, data: data)
     }
 }
 
 protocol HttpPostClient {
-    func post(url: URL)
+    func post(to: URL, data: Data?)
 }
 
 final class RemoteAddAccountTests: XCTestCase {
@@ -24,19 +26,31 @@ final class RemoteAddAccountTests: XCTestCase {
     func test_add_should_call_httpClient_with_correct_url() {
         let url = URL(string: "http://any-url.com")!
         let httpClientSpy = HttpClientSpy()
+        let addAccountModel = AddAccountModel(name: "any_name", email: "any_email", password: "any_password", passwordConfirmation: "any_passwordConfirmation")
         let sut = RemoteAddAccount(url: url, httpClient: httpClientSpy)
-        sut.add()
+        sut.add(addAccountModel: addAccountModel)
         
         XCTAssertEqual(httpClientSpy.url, url)
+    }
+    
+    func test_add_should_call_httpClient_with_correct_data() {
+        let httpClientSpy = HttpClientSpy()
+        let sut = RemoteAddAccount(url: URL(string: "http://any-url.com")!, httpClient: httpClientSpy)
+        let addAccountModel = AddAccountModel(name: "any_name", email: "any_email", password: "any_password", passwordConfirmation: "any_passwordConfirmation")
+        sut.add(addAccountModel: addAccountModel)
+        let data = try? JSONEncoder().encode(addAccountModel)
+        XCTAssertEqual(httpClientSpy.data, data)
     }
 }
 
 extension RemoteAddAccountTests {
     class HttpClientSpy: HttpPostClient {
         var url: URL?
+        var data: Data?
         
-        func post(url: URL) {
-            self.url = url
+        func post(to: URL, data: Data?) {
+            self.url = to
+            self.data = data
         }
     }
 }
--- Domain/AddAccount.swift
+++ Domain/AddAccount.swift
@@ -4,11 +4,18 @@ public protocol Account {
     func add(AddAccountModel: AddAccountModel, completion: @escaping (Result<AccountModel, Error>)-> Void )
 }
 
-public struct AddAccountModel {
+public struct AddAccountModel: Encodable {
     public var name: String
     public var email: String
     public var password: String
     public var passwordConfirmation: String
+    
+    public init(name: String, email: String, password: String, passwordConfirmation: String) {
+        self.name = name
+        self.email = email
+        self.password = password
+        self.passwordConfirmation = passwordConfirmation
+    }
 }
 
 
