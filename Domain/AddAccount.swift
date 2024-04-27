c434557f3cc74ebe0198834c3c80d323ac8c6220
Leticia <leticiaspeda@gmail.com>
2024-04-27 18:45:29 -0300

    chore: transforma o domain em caso de uso e models publicas

--- Domain/AddAccount.swift
+++ Domain/AddAccount.swift
@@ -1,14 +1,14 @@
 import Foundation
 
-protocol Account {
+public protocol Account {
     func add(AddAccountModel: AddAccountModel, completion: @escaping (Result<AccountModel, Error>)-> Void )
 }
 
-struct AddAccountModel {
-    var name: String
-    var email: String
-    var password: String
-    var passwordConfirmation: String
+public struct AddAccountModel {
+    public var name: String
+    public var email: String
+    public var password: String
+    public var passwordConfirmation: String
 }
 
 
