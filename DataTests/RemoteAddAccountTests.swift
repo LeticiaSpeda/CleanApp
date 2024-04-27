bcae94d497e6f2174ec07d1158d2d503f8855955
Leticia <leticiaspeda@gmail.com>
2024-04-27 18:27:54 -0300

    test: garantindo RemoteAddAccount que esta chamando o HttpClient com a url correta

---
+++ DataTests/RemoteAddAccountTests.swift
@@ -0,0 +1,40 @@
+import XCTest
+
+
+class RemoteAddAccount {
+    private let url: URL
+    private let httpClient: HttpPostClient
+    
+    init(url: URL, httpClient: HttpPostClient) {
+        self.url = url
+        self.httpClient = httpClient
+    }
+    
+    func add() {
+        httpClient.post(url: url)
+    }
+}
+
+protocol HttpPostClient {
+    func post(url: URL)
+}
+
+final class RemoteAddAccountTests: XCTestCase {
+
+    func test() {
+        let url = URL(string: "http://any-url.com")!
+        let httpClientSpy = HttpClientSpy()
+        let sut = RemoteAddAccount(url: url, httpClient: httpClientSpy)
+        sut.add()
+        
+        XCTAssertEqual(httpClientSpy.url, url)
+    }
+    
+    class HttpClientSpy: HttpPostClient {
+        var url: URL?
+        
+        func post(url: URL) {
+            self.url = url
+        }
+    }
+}
