d54460f5c0a42b71e289e7fc4502633a9a5dafff
Leticia <leticiaspeda@gmail.com>
2024-05-01 12:59:13 -0300

    test: garantindo que o RemoteAddAccount chame o HttpClientapenas uma vez

--- DataTests/UseCases/RemoteAddAccountTests.swift
+++ DataTests/UseCases/RemoteAddAccountTests.swift
@@ -11,7 +11,7 @@ final class RemoteAddAccountTests: XCTestCase {
         let (sut, httpClientSpy) = makeSut(url: url)
         sut.add(addAccountModel: makeAddAccountModel())
         
-        XCTAssertEqual(httpClientSpy.url, url)
+        XCTAssertEqual(httpClientSpy.urls, [url])
     }
     
     func test_add_should_call_httpClient_with_correct_data() {
@@ -35,12 +35,14 @@ extension RemoteAddAccountTests {
     }
     
     class HttpClientSpy: HttpPostClient {
-        var url: URL?
+        var urls = [URL]()
         var data: Data?
+        var callsCount = 0
         
-        func post(to: URL, data: Data?) {
-            self.url = to
+        func post(to url: URL, data: Data?) {
+            self.urls.append(url)
             self.data = data
+            self.callsCount += 1
         }
     }
 }
